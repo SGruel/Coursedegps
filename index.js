f7eeaa41564e1d604207e8ae88e5085b2ba9b543
SGruel <simeon.gruel@gmail.com>
2017-12-11 22:48:28 +0100

    coucou Zarz

--- index.js
+++ index.js
@@ -10,24 +10,23 @@ var valid;
 
 // variable globale
 var valid = document.getElementById('valid')
+var formspeed = document.getElementById('formspeed')
 //initialisation de la page
 window.onload =init;
 
 
 
 
-function traceGPS(){
-  formspeed.style.visibility='visible'
 
-};
 
 function init(){
 
    // initialisation des variables
    var map = L.map('map').setView([51.505, -0.09], 13);
    var formspeed = document.getElementById('formspeed')
-   var valid = document.getElementById('valid')
 
+   var valid = document.getElementById('valid')
+   var request= new XMLHttpRequest()
 
    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -35,8 +34,29 @@ function init(){
    // cache du formulaire des vitesses
    formspeed.style.visibility='hidden';
 
+
+
+
+
+
+
    valid.addEventListener('click',traceGPS);
 
 };
 
 // affichage de la course + formulaire des vitesses
+
+
+
+function traceGPS(){
+  formspeed.style.visibility='visible';
+};
+
+function  Goajax(ajax){
+  if  (ajax.readyState ==4 && ajax.status ==200){
+    console.log('prout' )
+  };
+};
+function trace_polyligne(listCoord){
+
+};
