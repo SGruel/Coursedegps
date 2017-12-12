49cf7aafe17fd87a1fd71f9d357237be131a7aed
azarz <amaury.zarzelli@ensg.eu>
2017-12-12 01:15:11 +0100

    bon

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-
-
- var selectionform= document.getElementById('test');
-
 //initialisation de la page
 window.onload = init;
+
+var selectionform = document.getElementById("test");
+var formspeed = document.getElementById("speeddiv");
 
 function init(){
 
    // initialisation des variables
+   selectionform = document.getElementById("test");
+	formspeed = document.getElementById("speeddiv");
 
    var valid=document.getElementById('valid');
    var map = L.map('map').setView([51.505, -0.09], 13);
-   var formspeed = document.getElementById('formspeed');
-   var selectionform= document.getElementById("test");
-
 
    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(map);
+
    // cache du formulaire des vitesses
    formspeed.style.visibility='hidden';
    selectionform.addEventListener('submit',traceGPS);
-   function traceGPS(){
-     formspeed.style.visibility='visible';
-   };
 
+}
 
-
-
-
-
-
-
-};
+function traceGPS(event){
+	event.preventDefault();
+ formspeed.style.visibility='visible';
+}
